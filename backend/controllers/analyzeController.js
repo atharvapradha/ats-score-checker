e8ac19075d8b723dc424e4f423630d6561cd1cec
Atharva Pradhan <atharvapradhan749@gmail.com>
2026-07-12 16:01:24 +0530

    Fix NLP endpoint URL

--- backend/controllers/analyzeController.js
+++ backend/controllers/analyzeController.js
@@ -3,6 +3,11 @@ const axios = require("axios");
 exports.analyzeResume = async (req, res) => {
   try {
     const { resumeText, jobDescription } = req.body;
+    console.log("Resume Text:");
+console.log(resumeText);
+
+console.log("Job Description:");
+console.log(jobDescription);
 
     // Call Python NLP service (use 127.0.0.1 instead of localhost)
     const response =await axios.post("https://ats-score-checker-nlp.onrender.com/analyze", {
